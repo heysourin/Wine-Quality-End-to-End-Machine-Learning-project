471551a5deb96c8cd4acebbad1b04629fd83b24d
heysourin <sourin07@gmail.com>
2024-03-31 16:50:34 +0530

    files added

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # End-to-end-ML-project
+
+```bash
+python3 -m venv myenv
+```
+
+```bash
+source myenv/bin/activate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python3 app.py
+```
+
+```bash
+Now open up your local host 0.0.0.0:8080
+```
+
+```bash
+deactivate
+```
---
+++ template.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "mlProject"
+
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    # separating file name and directory
+    filedir, filename = os.path.split(filepath)
+
+    # logic for creating the folder
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    #! logic for creating the file
+    # If the file doesnot exist or filepath size == 0(empty), then create and log
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f: #??
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
+
+# ? The with statement ensures that the file is properly closed after writing.
