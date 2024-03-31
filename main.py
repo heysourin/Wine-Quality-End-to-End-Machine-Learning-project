85532e107da3352ac82e5efd1036f90517fb39fb
heysourin <sourin07@gmail.com>
2024-03-31 17:17:10 +0530

    Logger file updated

---
+++ main.py
@@ -0,0 +1,3 @@
+from mlProject import logger
+
+logger.info("This is our custom log")
---
+++ src/mlProject/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+
+log_filepath = os.path.join(log_dir, "running_log.log")
+os.makedirs(log_dir, exist_ok=True)  # Create directory if not exists
+
+logging.basicConfig(
+
+    level=logging.INFO,
+    format=logging_str,
+
+    handlers=(
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    )
+)
+
+logger = logging.getLogger("mlProject")
+
+""""
+#? logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+1. saves the current timestamp and current date
+2. Then it will save the level name from what particular level you are running the logging file from
+3. Then it will give you the module name.
+4. Finally it shows the logging message.
+"""
