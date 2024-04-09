23698b096a84d56dd0dc9822456338463d2539db
heysourin <sourin07@gmail.com>
2024-04-10 00:14:10 +0530

    Bhai g phat gya to seup data pipeline 😫

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,7 +9,7 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -20,7 +21,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
     obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation (Train-Test-Split) Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
